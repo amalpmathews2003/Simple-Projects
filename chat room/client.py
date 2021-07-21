2195cdd64ba8bba2fe210817ada221e7d5219b9c
Amal P Mathews <amalpmathews2003@gmail.com>
2021-07-21 14:53:56 +0530

    Add files via upload

---
+++ chat room/client.py
@@ -0,0 +1,32 @@
+import threading
+import socket
+
+nickname=input('choose a nickname : ')
+
+client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('192.168.43.12',55555))
+
+def recieve():
+	while True:
+		try:
+			message = client.recv(1024).decode('ascii')
+			if message =='nick':
+				client.send(nickname.encode('ascii'))
+			else:
+				print(message)
+		except:
+			print('unknown error ocuured')
+			client.close()
+			break
+
+def write():
+	while True:
+		message = f'{nickname}	:{input("")}'
+		client.send(message.encode('ascii'))
+
+
+recieve_thread=threading.Thread(target=recieve)
+recieve_thread.start()
+
+write_thread=threading.Thread(target=write)
+write_thread.start()
